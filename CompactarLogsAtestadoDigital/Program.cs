d9185e01ae2b437eecb690081da70d740e8acb52
Maycon Wisley Batista <maycon.batista@seniornortepr.com.br>
2025-01-20 14:34:05 -0300

    Add project files.

---
+++ CompactarLogsAtestadoDigital/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace CompactarLogsAtestadoDigital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathFilesLogs;
+
+            // Verifica se tem o parametro de entra do log
+            if (args.Length > 0)
+            {
+                pathFilesLogs = args[0];
+            }
+            else
+            {
+                return;
+            }
+            string zipPath = Path.Combine(pathFilesLogs, $@"{DateTime.Now:yyyy_MM} Logs Compentencia.zip");
+            var filesDirectory = Directory.GetFiles(pathFilesLogs, "*.txt", SearchOption.TopDirectoryOnly);
+
+            // Cria um arquivo zip vazio
+            using (FileStream zipToOpen = new FileStream(zipPath, FileMode.Create))
+            {
+                using (ZipArchive archive = new ZipArchive(zipToOpen, ZipArchiveMode.Create))
+                {
+                    foreach (var file in filesDirectory)
+                    {
+                        // Adiciona arquivos individualmente
+                        archive.CreateEntryFromFile(file, Path.GetFileName(file));
+                    }
+                }
+            }
+        }
+    }
+}
